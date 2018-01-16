30dc189797ec81e8178303855b7009a332e8ee46
Chen Yu Pao <windperson@gmail.com>
2018-01-16 18:34:11 +0800

    TBD

---
+++ XamarinFormBGdemo/XamarinFormBGdemo.Android/PeriodicService.cs
@@ -0,0 +1,22 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Xamarin.Forms;
+
+namespace XamarinFormBGdemo.Droid
+{
+    public class PeriodicService : Service
+    {
+        public override IBinder OnBind(Intent intent)
+        {
+            return null;
+        }
+
+        public override StartCommandResult OnStartCommand(Intent intent, StartCommandFlags flags, int startId)
+        {
+            MessagingCenter.Send<object, string>(this, "UpdateLabel", "Hello from Android");
+
+            return StartCommandResult.NotSticky;
+        }
+    }
+}
---
+++ XamarinFormBGdemo/XamarinFormBGdemo/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinFormBGdemo
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+
+		    MessagingCenter.Subscribe<object, string>(this, "UpdateLabel", (s, e) =>
+		    {
+		        Device.BeginInvokeOnMainThread(() =>
+		        {
+		            BackgroundServiceLabel.Text = e;
+		        });
+		    });
+        }
+	}
+}
